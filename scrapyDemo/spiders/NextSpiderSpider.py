a348b909b392d70fd979b9069290cf447f5d9cd5
zsy <zsy1988cool@163.com>
2021-02-23 10:11:31 +0800

    项目初始化

---
+++ scrapyDemo/spiders/NextSpiderSpider.py
@@ -0,0 +1,35 @@
+import scrapy
+
+from scrapyDemo.items import MyspiderItem
+
+
+class NextSpiderSpider(scrapy.Spider):
+    name = "nextSpider"
+    allowed_domains = ["lab.scrapyd.cn"]
+    start_urls = ['http://lab.scrapyd.cn/']
+
+    def parse(self, response):
+        content = response.css('div.quote')  # 提取首页所有名言，保存至变量content
+        for v in content:  # 循环获取每一条名言里面的：名言内容、作者、标签
+            text = v.css('.text::text').extract_first()  # 提取名言
+            author = v.css('.author::text').extract_first()  # 提取作者
+            tags = v.css('.tags .tag::text').extract()  # 提取标签
+            tags = ','.join(tags)  # 数组转换为字符串
+            """
+            接下来进行写文件操作，每个名人的名言储存在一个txt文档里面
+            """
+            file_name = '%s-语录.txt' % author  # 定义文件名,如：木心-语录.txt
+
+            item = MyspiderItem()
+            item['text'] = text
+            item['author'] = author
+            item['tags'] = tags
+            # 将爬取的数据写成txt文件
+            with open(file_name, "a+") as f:  # 不同人的名言保存在不同的txt文档，“a+”以追加的形式
+                f.write(text)
+                f.write('\n')  # ‘\n’ 表示换行
+                f.write('标签：' + tags)
+                f.write('\n-------\n')
+                f.close()
+            # scrapy crawl nextSpider -o items.json -s FEED_EXPORT_ENCODING=UTF-8 将数据导出来序列化变成json格式
+            yield item
